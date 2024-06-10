fae321e3fdcd0d2e52935820ddc2f367c982273e
Đoàn Thành Lợi <100418351+ThanhLoi-300@users.noreply.github.com>
2024-06-10 13:29:19 +0700

    deploy backend

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -30,7 +30,7 @@ module.exports = {
       const activationToken = createActivationToken(user);
 
       //const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
-      const activationUrl = `http://localhost:5173/activation/${activationToken}`;
+      const activationUrl = `https://ecommerce-mern-frontend-fv9k.onrender.com/activation/${activationToken}`;
 
       try {
         await sendMail({
